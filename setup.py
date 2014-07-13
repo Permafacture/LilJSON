ed922936b2637273e7430345387b1d5beb834b97
Elliot <elbiot@elbitop>
2014-07-13 17:42:16 -0500

    made installable by pip, added .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+name = 'liljson'
+setup(name=name,
+    version='1.0',
+    author='Flávio Codeço Coelho',
+    url='https://github.com/Permafacture/LilJSON',
+    packages=[name],
+    long_description=open('readme.md').read()
+    )
